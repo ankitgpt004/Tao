b1516cb6fc1be3cf6fd9c46da66611da7e6e289b
ankitgpt004 <ankitgpt004@users.noreply.github.com>
2024-05-15 01:42:35 +0530

    Update TaoService.java online

--- tao/src/main/java/com/example/tao/service/TaoService.java
+++ tao/src/main/java/com/example/tao/service/TaoService.java
@@ -1,14 +1,21 @@
 package com.example.tao.service;
 
 import com.example.tao.entity.Product;
+
+import java.time.LocalDateTime;
 import java.util.List;
 
 public interface TaoService {
 	
+	List<Product> getProductByActiveStatus();
+	List<Product> getProductByName(String productName);
+	List<Product> getProductBypriceRange(int minPrice, int maxPrice);
+	List<Product> getProductByDateRange(LocalDateTime minPostedDate, LocalDateTime maxPostedDate);
 	Product saveProductData(Product product);
-	List<Product> getProducts();
 	Product updateProduct(Product product);
 	void deleteProduct(Long id);
-	List<Product> getProductByActiveStatus();
-
+	List<Product> getproductsInApprovalQueue();
+	void approveProduct(long productId);
+	void rejectProduct(long productId);
+	
 }
