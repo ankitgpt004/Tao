750ecc106beebf93747597bcd2d6872e92ba51a9
ankitgpt004 <ankitgpt004@users.noreply.github.com>
2024-05-15 01:46:49 +0530

    Update ApprovalQueueRepository.java

--- tao/src/main/java/com/example/tao/repo/ApprovalQueueRepository.java
+++ tao/src/main/java/com/example/tao/repo/ApprovalQueueRepository.java
@@ -2,7 +2,9 @@ package com.example.tao.repo;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.example.tao.entity.ApprovalQueue;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ApprovalQueueRepository extends JpaRepository<ApprovalQueue, Long>{
 
 }
