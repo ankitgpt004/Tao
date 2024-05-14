b5004ecae228338bde9f638baa4798fce4b7438d
ankitgpt004 <ankitgpt004@users.noreply.github.com>
2024-05-15 01:40:55 +0530

    Add files via upload

---
+++ tao/src/main/java/com/example/tao/entity/ApprovalQueue.java
@@ -0,0 +1,32 @@
+package com.example.tao.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "approval_queue")
+public class ApprovalQueue {
+	
+	@Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long productId;
+	private String productName;
+	public Long getProductId() {
+		return productId;
+	}
+	public void setProductId(Long productId) {
+		this.productId = productId;
+	}
+	public String getProductName() {
+		return productName;
+	}
+	public void setProductName(String productName) {
+		this.productName = productName;
+	}
+	
+	
+
+}
